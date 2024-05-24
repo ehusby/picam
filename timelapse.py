43096608942078d572208f18cddb0f32836ea1d3
Erik Husby <20846681+ehusby@users.noreply.github.com>
2024-05-25 02:43:50 +0800

    Use proper sunrise and sunset times for capture window

--- timelapse.py
+++ timelapse.py
@@ -8,6 +8,14 @@ import subprocess
 import time
 from picamera import PiCamera
 
+from suntime import Sun
+
+
+LOCAL_TIME_ZONE = datetime.datetime.now(datetime.timezone.utc).astimezone().tzinfo
+
+SUN = None
+YOUTUBE_PLAYLIST_NAME = None
+
 
 def save_video(vid_date, output_directory):
     input_path_pattern = f"{output_directory}/{vid_date.strftime('IMG_%Y%m%d')}*.jpg"
@@ -20,8 +28,8 @@ def save_video(vid_date, output_directory):
         },
         {
             "out_path": f"{output_directory}/{vid_date.strftime('VID_%Y%m%d_TS.mp4')}",
-            "cmd": """ ffmpeg -loglevel error -framerate 30 -pattern_type glob -i '{in_path}' -c:v libx264 -preset ultrafast -pix_fmt yuv420p -vf "drawtext=fontfile=/Library/Fonts/Arial\ Unicode.ttf: fontsize=30: fontcolor=white: text='%{{metadata\:DateTime\:def_value}}': x=(w-tw)/2: y=h-(2*lh)" '{out_path}' """,
-            "upload_title": vid_date.strftime('%Y-%m-%d %A'),
+            "cmd": """ ffmpeg -loglevel error -framerate 30 -pattern_type glob -i '{in_path}' -c:v libx264 -preset ultrafast -pix_fmt yuv420p -vf "drawtext=fontfile=/Library/Fonts/Arial\ Unicode.ttf: fontsize=84: fontcolor=white: text='%{{metadata\:DateTime\:def_value}}': x=(w-tw)/2: y=h-(2*lh)" '{out_path}' """,
+            "upload_title": vid_date.strftime('%Y-%m-%d %A') if YOUTUBE_PLAYLIST_NAME is not None else None,
         },
     ]
 
@@ -43,7 +51,7 @@ def save_video(vid_date, output_directory):
             upload_title = run["upload_title"]
             if upload_title is not None and proc.returncode == 0:
                 print(f"Uploading to YouTube with video title '{upload_title}': {out_path}")
-                proc = subprocess.run(f""" youtube-upload --privacy="unlisted" --title="{upload_title}" --playlist="Ubud Sawah Timelapses" "{out_path}" """, shell=True, timeout=600)
+                proc = subprocess.run(f""" youtube-upload --privacy="unlisted" --title="{upload_title}" --playlist="{YOUTUBE_PLAYLIST_NAME}" "{out_path}" """, shell=True, timeout=600)
 
     if success and all(os.path.isfile(f) for f in [run["out_path"] for run in run_list]):
         print(f"Deleting daily image files: {input_path_pattern}")
@@ -62,16 +70,19 @@ def capture_photos(interval_sec, output_directory):
 
     try:
         while True:
-            now = datetime.datetime.now()
-            # now = datetime.datetime(year=2024, month=5, day=11, hour=22)
+            now = datetime.datetime.now(tz=LOCAL_TIME_ZONE)
+            # now = datetime.datetime(year=2024, month=5, day=11, hour=14, tzinfo=LOCAL_TIME_ZONE)
+
+            sunrise_time = SUN.get_sunrise_time(at_date=now, time_zone=LOCAL_TIME_ZONE)
+            sunset_time = SUN.get_sunset_time(at_date=now, time_zone=LOCAL_TIME_ZONE)
 
             if now.date() == ended_day:
                 pass
 
-            elif now.time() < datetime.time(hour=5, minute=45):
+            elif now.time() < (sunrise_time - datetime.timedelta(minutes=40)).time():
                 pass
 
-            elif now.time() > datetime.time(hour=19):
+            elif now.time() > (sunset_time + datetime.timedelta(minutes=40)).time():
                 ended_day = now.date()
                 input_path_pattern = f"{output_directory}/{ended_day.strftime('IMG_%Y%m%d')}*.jpg"
                 if len(glob.glob(input_path_pattern)) > 0:
@@ -93,6 +104,9 @@ def capture_photos(interval_sec, output_directory):
 
 
 def main():
+    global SUN
+    global YOUTUBE_PLAYLIST_NAME
+
     parser = argparse.ArgumentParser(
         description="Capture timelapse photos with a Raspberry Pi camera and create 30 FPS daily videos.",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
@@ -109,11 +123,31 @@ def main():
         required=True,
         help="Directory to save captured photos and videos",
     )
+    parser.add_argument(
+        "-lat", "--latitude",
+        type=float,
+        required=True,
+        help="Latitude of camera location",
+    )
+    parser.add_argument(
+        "-lon", "--longitude",
+        type=float,
+        required=True,
+        help="Longitude of camera location",
+    )
+    parser.add_argument(
+        "--youtube-playlist",
+        type=str,
+        help="Upload timestamped video to YouTube playlist with this name (requires youtube-upload install: https://github.com/tokland/youtube-upload)",
+    )
     args = parser.parse_args()
 
     args.output_directory = os.path.expanduser(args.output_directory)
     if not os.path.isdir(args.output_directory):
         parser.error(f"Output directory does not exist: {args.output_directory}")
+
+    SUN = Sun(args.latitude, args.longitude)
+    YOUTUBE_PLAYLIST_NAME = args.youtube_playlist
 
     capture_photos(args.interval, args.output_directory)
 
